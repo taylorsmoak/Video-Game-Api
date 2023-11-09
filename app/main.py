995aaaea76da58e2d37a1f3955298f9d5ed970b8
smoak88 <smoak88@gmail.com>
2023-11-09 14:34:59 -0800

    Add and configure CORSMiddleware

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .database import Base, engine
 from .routers import titles, ids, release_dates, ratings, genres
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
 
 app.include_router(titles.router)
 app.include_router(ids.router)
